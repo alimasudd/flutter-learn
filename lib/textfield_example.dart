3c33c7ca011876fcbb267901f1251a90872810d9
androidProject <alimasudd.dev@gmail.com>
2020-10-08 13:04:28 +0700

    textfield

--- lib/textfield_example.dart
+++ lib/textfield_example.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TextfieldExample extends StatefulWidget {
@@ -18,26 +19,12 @@ class _TextfieldExampleState extends State<TextfieldExample> {
         body: Container(
           margin: EdgeInsets.all(20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+//            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.blue,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    icon: Icon(Icons.add_call),
-                    prefixIcon: Icon(Icons.person),
-//                    prefixText: "Name : ",
-                    prefixStyle: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold),
-                    labelText: "Nama Lengkap",
-                    hintText: "Nama Lengkap Anda"),
-                maxLength: 20,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
-              ),
+              _TextField(20, Colors.blue, Icons.person, Icons.person_add,
+                  "Nama Lengkap"),
+              _TextField(200, Colors.deepOrangeAccent, Icons.location_on,
+                  Icons.location_city, "Alamat"),
               Text(
                 controller.text,
                 style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
@@ -46,6 +33,26 @@ class _TextfieldExampleState extends State<TextfieldExample> {
           ),
         ),
       ),
+    );
+  }
+
+  TextField _TextField(int max, Color color, IconData iconData1,
+      IconData iconData2, String text1) {
+    return TextField(
+      decoration: InputDecoration(
+          fillColor: color,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          icon: Icon(iconData1),
+          prefixIcon: Icon(iconData2),
+//                    prefixText: "Name : ",
+          prefixStyle: TextStyle(color: color, fontWeight: FontWeight.bold),
+          labelText: text1,
+          hintText: text1),
+      maxLength: max,
+      onChanged: (value) {
+        setState(() {});
+      },
+      controller: controller,
     );
   }
 }
